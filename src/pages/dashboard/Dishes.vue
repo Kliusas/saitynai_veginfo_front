ade0dc2c87f50af7afb7b20ffa9c8153a328477c
Kliusas <karolis.kliucnikas@hostinger.com>
2019-12-09 05:32:05 +0200

    Update

--- src/pages/dashboard/Dishes.vue
+++ src/pages/dashboard/Dishes.vue
@@ -1,57 +1,183 @@
 <template>
   <div>
-    <md-field>
-      <label>dishesCategoryName</label>
-      <md-input v-model="storeDishesCategoryName"></md-input>
-    </md-field>
-    <md-button @click="storeDishesCategory">Default</md-button>
-    <md-field>
-      <label>dishesCategoryName</label>
-      <md-input v-model="updateDishesCategoryName"></md-input>
-    </md-field>
-    <md-button @click="updateDishesCategory">Default</md-button>
+    <navigation></navigation>
+    <transition-group
+      class="container d-flex flex-wrap"
+      name="fade"
+      mode="out-in"
+    >
+      <div class="mb-3 col-12" key="deleteBlock">
+        <portlet horizontal-full>
+          <template slot="title"
+            >Want to add new item?</template
+          >
+          <template slot="content">
+            <button
+              @click="showModal('create')"
+              class="text-white btn btn-primary m-1 m-md-0 ml-0 ml-md-3"
+            >
+              Create
+            </button>
+          </template>
+        </portlet>
+      </div>
+      <div
+        class="mb-5 col-12 col-md-6 d-flex justify-content-center"
+        v-for="type in refreshData"
+        :key="type.id"
+      >
+        <portlet horizontal>
+          <template slot="title">{{ type.name }}</template>
+          <template slot="content">
+            <div>
+              <button
+                @click="showModal('edit', type)"
+                class="text-white btn btn-primary m-1 m-md-0 ml-0 ml-md-3"
+              >
+                Edit
+              </button>
+              <button
+                @click="deleteItem(type.id)"
+                class="text-white btn btn-danger m-1 m-md-0 ml-0 ml-md-3"
+              >
+                Delete
+              </button>
+            </div>
+          </template>
+        </portlet>
+      </div>
+    </transition-group>
+    <modal
+      v-if="modalEdit"
+      :title="modalEdit.header"
+      :data="modalEdit.data"
+      :id="modalEdit.id"
+      :show.sync="modalEdit.show"
+    >
+      <template slot="modal-body" slot-scope="props">
+        <input
+          type="text"
+          class="form-control mt-4 mb-4"
+          v-model="props.data.name"
+        />
+        <input
+          type="text"
+          class="form-control mt-4 mb-4"
+          v-model="props.data.description"
+        />
+      </template>
+      <template slot="modal-footer" slot-scope="props">
+        <button class="btn btn-danger mr-1" @click="props.methods.close">
+          Cancel
+        </button>
+        <button class="btn btn-info" @click="updateData(props.data)">
+          Save
+        </button>
+      </template>
+    </modal>
+    <modal
+      v-if="modalCreate"
+      :title="modalCreate.header"
+      :data="modalCreate.data"
+      :id="modalCreate.id"
+      :show.sync="modalCreate.show"
+    >
+      <template slot="modal-body" slot-scope="props">
+        <p>Enter Dishes Name</p>
+        <input
+          type="text"
+          class="form-control mt-4 mb-4"
+          v-model="props.data.name"
+        />
+        <input
+          type="text"
+          class="form-control mt-4 mb-4"
+          v-model="props.data.description"
+        />
+      </template>
+      <template slot="modal-footer" slot-scope="props">
+        <button class="btn btn-danger mr-1" @click="props.methods.close">
+          Cancel
+        </button>
+        <button class="btn btn-info" @click="submitData(props.data)">
+          Create
+        </button>
+      </template>
+    </modal>
   </div>
 </template>
-
 <script>
+import Navigation from "../../components/Navigation";
+import Portlet from "../../components/Portlet";
+import Modal from "../../components/Modal";
 export default {
-  name: "Dishes",
-  props: {},
+  name: "dishes",
+  components: { Modal, Portlet, Navigation },
   data() {
     return {
-      storeDishesCategoryName: "",
-      updateDishesCategoryName: ""
+      data: [],
+      show: false,
+      modalEdit: {
+        header: "Edit Dishes",
+        data: {},
+        id: "edit-dishes",
+        show: false
+      },
+      modalCreate: {
+        header: "Create Dishes",
+        data: {},
+        id: "create-dishes",
+        show: false
+      }
     };
   },
-  mounted() {
+  created() {
     this.$http
-      .get("http://localhost/dishes-categories")
-      .then(response => console.log(response.data));
+      .get("http://localhost/dishes")
+      .then(response => (this.data = response.data));
+  },
+  computed: {
+    refreshData() {
+      return this.data;
+    }
   },
   methods: {
-    storeDishesCategory() {
-      this.$http
-        .post("http://localhost/dishes-categories", {
-          data: { name: this.storeDishesCategoryName }
-        })
-        .then(response => console.log(response.data))
-        .catch(err => console.warn(err));
+    deleteItem(id) {
+      this.$http.delete(`http://localhost/dishes/${id}`).then(() => {
+        this.data.splice(this.data.findIndex(x => x.id === id), 1);
+      });
     },
+    showModal(modalType, type = null) {
+      if (modalType === "create") {
+        this.modalCreate.show = true;
+      } else if (modalType === "edit") {
+        this.modalEdit.show = true;
+        this.modalEdit.data = type;
+      }
 
-    updateDishesCategory() {
+      window.document.body.style.overflowY = "hidden";
+    },
+    updateData(item) {
+      this.$http.patch(`http://localhost/dishes/${item.id}`, {
+        name: item.name,
+        description: item.desctiption
+      });
+      this.modalEdit.show = false;
+    },
+    submitData(item) {
+      this.modalCreate.show = false;
+
       this.$http
-        .patch(
-          "http://localhost/dishes-categories/:id",
-          { id: 2 },
-          {
-            name: this.updateDishesCategoryName
-          }
-        )
-        .then(response => console.log(response.data));
+        .post("http://localhost/dishes", {
+          data: { name: item.name, description: item.desctiption }
+        })
+        .finally(() => {
+          item.name = "";
+          this.$http.get("http://localhost/dishes").then(response => {
+            this.data = response.data;
+          });
+        });
     }
   }
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
--- src/pages/dashboard/categories/Elements.vue
+++ src/pages/dashboard/categories/Elements.vue
@@ -1,47 +1,44 @@
 <template>
-    <div>
-        <navigation></navigation>
-        <div class="container d-flex flex-wrap">
-            <div
-                    class="mb-5 col-12 col-md-6 d-flex justify-content-center"
-                    v-for="type in types"
-                    :key="type.url"
+  <div>
+    <navigation></navigation>
+    <div class="container d-flex flex-wrap">
+      <div
+        class="mb-5 col-12 col-md-6 d-flex justify-content-center"
+        v-for="type in types"
+        :key="type.url"
+      >
+        <portlet horizontal>
+          <template slot="title">{{ type.title }}</template>
+          <template slot="content">
+            <button
+              @click="$router.replace({ name: type.url }).catch(err => err)"
+              class="text-white btn btn-primary mt-3 mt-md-0 ml-0 ml-md-3"
             >
-                <portlet horizontal>
-                    <template slot="title">{{ type.title }}</template>
-                    <template slot="content">
-                        <button
-                                @click="$router.replace({ name: type.url }).catch(err => err)"
-                                class="text-white btn btn-primary mt-3 mt-md-0 ml-0 ml-md-3"
-                        >
-                            Manage
-                        </button>
-                    </template>
-                </portlet>
-            </div>
-        </div>
+              Manage
+            </button>
+          </template>
+        </portlet>
+      </div>
     </div>
+  </div>
 </template>
 <script>
-    import Portlet from "../../../components/Portlet";
-    import Navigation from "../../../components/Navigation";
-    export default {
-        name: "Elements",
-        components: { Navigation, Portlet },
-        data() {
-            return {
-                types: [
-                    { title: "Dishes", url: "dishes" },
-                    { title: "Events", url: "events" },
-                    { title: "Ingredients", url: "ingredients" },
-                    { title: "Products", url: "products" },
-                    { title: "Recipes", url: "recipes" },
-                    { title: "Restaurants", url: "restaurants" },
-                    { title: "Shop Products", url: "shop-products" },
-                    { title: "Shops", url: "shops" }
-                ]
-            };
-        }
+import Portlet from "../../../components/Portlet";
+import Navigation from "../../../components/Navigation";
+export default {
+  name: "Elements",
+  components: { Navigation, Portlet },
+  data() {
+    return {
+      types: [
+        { title: "Dishes", url: "dishes" },
+        { title: "Events", url: "events" },
+        { title: "Products", url: "products" },
+        { title: "Recipes", url: "recipes" },
+        { title: "Shop Products", url: "shop-products" }
+      ]
     };
+  }
+};
 </script>
 <style lang="scss"></style>
